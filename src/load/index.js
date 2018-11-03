e8fd8cd8767c81ea431283fb6fa030701c6c6cc4
William <william@prefinem.com>
2018-11-02 19:41:43 -0500

    move string load into it's own function

--- src/load/index.js
+++ src/load/index.js
@@ -1,15 +1,19 @@
+const stringLoad = (loadInfo) => {
+	if (loadInfo.indexOf('data:image') > -1 || loadInfo.indexOf('base64') > -1) {
+		return Buffer.from(loadInfo.replace(/^data:image\/\w+;base64,/u, ''), 'base64');
+	} else if (loadInfo.indexOf('http') > -1) {
+		return require('./url')(loadInfo);
+	}
+	return require('fs').readFileSync(loadInfo);
+};
+
 const load = (loadInfo, options) => {
 	if (loadInfo instanceof Buffer) {
 		return loadInfo;
 	} else if (loadInfo && loadInfo.key && loadInfo.bucket) {
 		return require('./s3')(loadInfo, options);
 	} else if (typeof loadInfo === 'string') {
-		if (loadInfo.indexOf('data:image') > -1 || loadInfo.indexOf('base64') > -1) {
-			return Buffer.from(loadInfo.replace(/^data:image\/\w+;base64,/u, ''), 'base64');
-		} else if (loadInfo.indexOf('http') > -1) {
-			return require('./url')(loadInfo);
-		}
-		return require('fs').readFileSync(loadInfo);
+		return stringLoad(loadInfo);
 	}
 
 	return undefined;
